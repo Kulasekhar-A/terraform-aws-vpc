4a64d007c13c8f3eb06835da2ed8909952ba540a
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-02-27 17:30:41 +0530

    actions

--- locals.tf
+++ locals.tf
@@ -4,11 +4,11 @@ locals {
         Environment = var.environment
         Terraform = "true"
     }
-    vpc_final_tags = merge {
+    vpc_final_tags = merge (
         local.common_tags, 
         {
         Name = "${var.project}-${var.environment}"
         },
         var.vpc_tags
-        }
+    )
 }
