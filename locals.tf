33c931d7638cd708754642b6d3767fecf6261e76
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-02-27 18:01:12 +0530

    actions

--- locals.tf
+++ locals.tf
@@ -14,7 +14,7 @@ locals {
     igw_final_tags = merge (
         local.common_tags,
         {
-            Name = "${var.project}-{var.environment}
+            Name = "${var.project}-${var.environment}"
         },
         var.igw_tags
     )
